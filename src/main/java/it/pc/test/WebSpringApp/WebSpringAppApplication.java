0ed60c6e337a0a7d1654ed3d2e398219583dad76
Samuel-C-02 <samuelciaghi@gmail.com>
2024-03-23 17:35:36 +0100

    SpringBoot Component Scan

--- src/main/java/it/pc/test/WebSpringApp/WebSpringAppApplication.java
+++ src/main/java/it/pc/test/WebSpringApp/WebSpringAppApplication.java
@@ -2,8 +2,10 @@ package it.pc.test.WebSpringApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "it.pc.test.WebSpringApp")
 public class WebSpringAppApplication {
 
 	public static void main(String[] args) {
