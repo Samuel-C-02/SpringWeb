7b8001b628d7d4bb6bceb44416861609ccff33be
Samuel-C-02 <samuelciaghi@gmail.com>
2025-03-21 17:07:13 +0100

    Added sql to create the schema and all its tables, and moved the sql that add values to those tables.

---
+++ db/data/ruolo_202503191850.sql
@@ -0,0 +1,3 @@
+INSERT INTO web.ruolo (id_ruolo,ruolo_utente) VALUES
+	 (1,'ROLE_USER'),
+	 (2,'ROLE_ADMIN');
---
+++ db/data/utente_202503191849.sql
@@ -0,0 +1,2 @@
+INSERT INTO web.utente (id_utente,username,email,"password",ruolo_id) VALUES
+	 (5,'Samuel-C-02','samuelc@gmail.com','$2a$10$VQob19j12WCNPlgpeiKkju5s4tERB3p1uJ31lTwjammIN9zZUc4vu',2);
